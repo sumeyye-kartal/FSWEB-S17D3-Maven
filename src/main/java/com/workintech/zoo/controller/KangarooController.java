e7004c3d4dc0d26365ffcf67289fa690583c9e88
sumeyye-kartal <smyyekartal@outlook.com>
2025-02-10 13:18:22 +0300

    done

---
+++ src/main/java/com/workintech/zoo/controller/KangarooController.java
@@ -0,0 +1,62 @@
+package com.workintech.zoo.controller;
+
+import com.workintech.zoo.entity.Kangaroo;
+import com.workintech.zoo.validations.ZooKangarooValidation;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/kangaroos")
+public class KangarooController {
+
+    private Map<Integer, Kangaroo> kangaroos;
+
+    @PostConstruct
+    public void init() {
+        kangaroos = new HashMap<>();
+    }
+
+    @GetMapping
+    public List<Kangaroo> find() {
+        return this.kangaroos.values().stream().toList();
+    }
+
+    @GetMapping("/{id}")
+    public Kangaroo find(@PathVariable("id") Integer id) {
+        ZooKangarooValidation.isValid(id);
+        ZooKangarooValidation.checkKangarooExistence(kangaroos,id,true);
+        return kangaroos.get(id);
+    }
+
+    @PostMapping
+    public Kangaroo save(@RequestBody Kangaroo kangaroo) {
+        ZooKangarooValidation.checkKangarooExistence(kangaroos, kangaroo.getId(), false);
+        ZooKangarooValidation.checkKangarooWeight(kangaroo.getWeight());
+        kangaroos.put(kangaroo.getId(), kangaroo);
+        return kangaroos.get(kangaroo.getId());
+    }
+
+    @PutMapping("/{id}")
+    public Kangaroo update(@PathVariable("id") Integer id, @RequestBody Kangaroo kangaroo) {
+        ZooKangarooValidation.isValid(id);
+        ZooKangarooValidation.checkKangarooWeight(kangaroo.getWeight());
+        kangaroo.setId(id);
+        if(kangaroos.containsKey(id)) {
+            kangaroos.put(id, kangaroo);
+            return kangaroos.get(id);
+        } else {
+            return save(kangaroo);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public Kangaroo delete(@PathVariable("id") Integer id) {
+        ZooKangarooValidation.isValid(id);
+        ZooKangarooValidation.checkKangarooExistence(kangaroos, id, true);
+        return kangaroos.remove(id);
+    }
+}
---
+++ src/main/java/com/workintech/zoo/exceptions/ZooErrorResponse.java
@@ -0,0 +1,21 @@
+package com.workintech.zoo.exceptions;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class ZooErrorResponse {
+
+    private Integer status;
+
+    private String message;
+
+    private Long timestamp;
+
+
+}
